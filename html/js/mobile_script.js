6e8e416af16f4ae768ecb658154b8c1041c53aac
sman <119931814+imsman@users.noreply.github.com>
2022-12-23 00:00:00 +0900

    반응형 수정

---
+++ html/js/mobile_script.js
@@ -0,0 +1,12 @@
+var count= 0;
+
+// 각각의 하트에 각각의 카운트넣어주기
+$('.heart-icon img').on('click',function(){
+    $(this).attr('src','../images/mo_icon_fullheart.svg');
+    count++;
+    console.log(count);
+
+    if(count%2 == 0){
+        $(this).attr('src','../images/mo_icon_heart.svg');
+    }
+});
